4fb59a22851a72b18c51cdd77f85c4b0370e6f24
Juanc4rl0ss <juan_c1985@hotmail.es>
2024-10-06 08:44:29 +0200

    Comenzamos con el tema de la interpolacion

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,11 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   // templateUrl: './empleado.component.html',
-  template: `<p>Aquí iría un empleado</p>`,
+  template: `<p>Me llamo {{nombre}} {{apellido}} y soy {{edad > 18 ? "Mayor de edad":"Menor de edad"}} años. Mi empresa es {{empresa}}</p>`,
   // styleUrl: './empleado.component.css'
-  styles: [`p{background-color: red}`]
+  styles: [`p{background-color: white; color: blue; padding: 10px; border: 1px solid black;}`]
   
 })
 export class EmpleadoComponent {
 
+  nombre = 'Juan';
+
+  apellido = 'Pérez';
+
+  edad = 18;
+
+  empresa = "Píldoras informáticas";
+
+  // getEdad() {
+  //   return this.edad;
+  // }
 }
